368ab646dc00510f171b0c23ad5d54e47716e9c8
chiru-1992 <chirugurung730@gmail.com>
2026-07-01 18:37:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,17 +5,18 @@ import {
   doc,
   setDoc,
   getDoc,
+  updateDoc,
   onSnapshot,
   collection
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDOwcDVPWwGerciKReQ4Vn_8nmVf0nuhjs",
+  authDomain: "portugal-croatia-tickets.firebaseapp.com",
+  projectId: "portugal-croatia-tickets",
+  storageBucket: "portugal-croatia-tickets.firebasestorage.app",
+  messagingSenderId: "537992386461",
+  appId: "1:537992386461:web:c889e03d7707ca51880174"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -27,6 +28,7 @@ export {
   doc,
   setDoc,
   getDoc,
+  updateDoc,
   onSnapshot,
   collection
 };
